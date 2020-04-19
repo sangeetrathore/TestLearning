c35da175b18004d3d98b4c64eb9a665fa9917319
sangeetrathore <63742349+sangeetrathore@users.noreply.github.com>
2020-04-19 17:45:06 +0530

    test

--- src/com/sangeet/StringFunctions/ConcatString.java
+++ src/com/sangeet/StringFunctions/ConcatString.java
@@ -15,6 +15,14 @@ public class ConcatString {
 
 		}
 
+		else if ("reverse".equalsIgnoreCase(firstarg)) {
+			String firstnumber = args[1];
+			//String secondnumber = args[2];
+			//String thirdnumber = args[3];
+
+			obj.reversenumber(firstnumber);
+
+		}
 	}
 
 	private void largestnumber(int a, int b, int c) {
@@ -30,4 +38,20 @@ public class ConcatString {
 		}
 	}
 
-}
+	private void reversenumber(String a) {
+		int d=a.length();
+		for (int i=1;i <= d;i++)  {
+		
+		int	c=Integer.parseInt(a);
+			
+		int b = c%10;
+		
+		System.out.print(b);
+		c=(c-b)/10;
+		a=c+"";
+		
+		}
+		}
+			
+		
+	}
